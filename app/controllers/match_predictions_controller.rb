10d1680006c8934bcf1e26b09e2d6c20e77c806a
luca gastaldi <lucagastaldi82@gmail.com>
2023-08-18 15:52:49 +0100

    fixed typo

--- app/controllers/match_predictions_controller.rb
+++ app/controllers/match_predictions_controller.rb
@@ -8,14 +8,14 @@ class MatchPredictionsController < ApplicationController
 
     answer1 = {home_score_guess: params["home_score_guess_0"], away_score_guess: params['away_score_guess_0'], user_id: params[:user_id] }
     answer2 = {home_score_guess: params["home_score_guess_1"], away_score_guess: params['away_score_guess_1'], user_id: params[:user_id] }
-    @match_prediction1 = MatchPrediction.new(answer1)
-    @match_prediction2 = MatchPrediction.new(answer2)
-    send_guesses(@match_prediction1)
-    # if @match_prediction.save
-    #   redirect_to match_predictions_path
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    @match_prediction = MatchPrediction.new(answer1)
+    # @match_prediction2 = MatchPrediction.new(answer2)
+    if @match_prediction.save
+      send_guesses(@match_prediction)
+      redirect_to match_predictions_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def send_guesses(guess)
