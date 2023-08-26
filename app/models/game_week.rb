9c9c0dab7c077476a5dad5864f26ecbef88c4996
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-08-26 18:26:14 +0100

    dependant: :nullify added to GameWeek matches to ensure that foreign key of matches is set to null on game_week.destroy

--- app/models/game_week.rb
+++ app/models/game_week.rb
@@ -1,5 +1,5 @@
 class GameWeek < ApplicationRecord
-  has_many :matches
+  has_many :matches, dependant: :nullify
 
   validates :start_date, :end_date, :week_number, presence: true
   validates :start_date, comparison: { less_than: :end_date }
