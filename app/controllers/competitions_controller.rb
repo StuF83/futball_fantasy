8ab44b5cde791a2bef0c1ea85eea604cbb848006
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-09-14 12:24:49 +0100

    competition table user email headders added

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -6,10 +6,10 @@ class CompetitionsController < ApplicationController
 
   def show
     @competition = Competition.find(params[:id])
-    @user_game_weeks = @competition.game_weeks
     @game_weeks = @competition.game_weeks
-    @users = @competition.users
-
+    @users = []
+    @competition.users.each {|user| @users << user.email}
+    @users.sort!
     # when getting all the predictions for the competition, check which one's have a result of pending.
     # if any of the resutls are pending, trigger a method to update those pending results.
 
