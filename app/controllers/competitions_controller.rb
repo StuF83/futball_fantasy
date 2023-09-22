deb217c3c771761f1be2daaca4e8c151962f01b2
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-09-22 18:27:33 +0100

    updated @competition_game_week includes call so continues to work when match_predictions are missing

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -22,7 +22,8 @@ class CompetitionsController < ApplicationController
   def show
     @competition = Competition.find(params[:id])
     # @users = @competition.users
-    @competition_game_weeks = GameWeek.joins(:matches => [:match_predictions => [:user => :competitions]]).where(:competitions => {:id => params[:id]})#.distinct
+    # @competition_game_weeks = GameWeek.includes(:matches => [:match_predictions => [:user => :competitions]]).where([:competition_game_weeks => :competitions] => {:id => params[:id]})
+    @competition_game_weeks = GameWeek.includes( :competitions => [:users], :matches => [:match_predictions] ).where(:competitions => {:id => params[:id]})
     @competition_game_weeks.each do |game_week|
       game_week.match_predictions.each do |match_prediction|
         if match_prediction.home_score_guess? || match_prediction.away_score_guess?
@@ -30,6 +31,7 @@ class CompetitionsController < ApplicationController
         end
       end
     end
+    # raise
   end
 
   def edit
