314d85792db96d56a9272c16f6456c4d431296e6
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-10-04 21:14:59 +0100

    order added to active record quiery for  competition page to ensure rows align

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -43,10 +43,8 @@ class CompetitionsController < ApplicationController
 
   def show
     @competition = Competition.find(params[:id])
-    @users = @competition.users.order(:id)
-    # a row is : match = [home_team, away_team, home_score, away_score] << user.match_prediction.home_guess and away guess
+    @competition_game_weeks = GameWeek.includes( :competitions => [:users], :matches => [:match_predictions] ).where(:competitions => {:id => params[:id]}).order(:id)
 
-    @competition_game_weeks = GameWeek.includes( :competitions => [:users], :matches => [:match_predictions] ).where(:competitions => {:id => params[:id]})
     @competition_game_weeks.each do |game_week|
       game_week.match_predictions.each do |match_prediction|
         if match_prediction.home_score_guess? || match_prediction.away_score_guess?
