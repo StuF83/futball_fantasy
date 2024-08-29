c6838737f30a82344bf9efa6c746f4cf31e067f6
Stuart <stuart.s.frew@gmail.com>
2024-08-29 10:06:12 +0100

    username required for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  before_action :configure_devise_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  private
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])  
+  end
 end
