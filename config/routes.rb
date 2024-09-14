81565d5d2468a352b09893cbc4a4920f39cf8c32
Stuart <stuart.s.frew@gmail.com>
2024-09-14 17:09:56 +0100

    pre generator removed

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   patch '/match_predictions/current_predictions_update/', to: 'match_predictions#current_predictions_update'
 
   #not for production
-  if Rails.env.development? || Rails.env.test?
-    get '/match_predictions/generate_predictions', to: 'match_predictions#generate_predictions'
-  end
+  # if Rails.env.development? || Rails.env.test?
+  #   get '/match_predictions/generate_predictions', to: 'match_predictions#generate_predictions'
+  # end
 
   resources :competitions, only: [:index, :show, :new, :create, :update, :edit] do
     resources :game_weeks, only: [:update]
